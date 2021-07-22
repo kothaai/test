89f0d6ba33efe1ff1f5bd4d98c0595ddedc3f7d5
kothaai <46052869+kothaai@users.noreply.github.com>
2021-07-22 18:03:11 +0530

    Update gitprint.java

--- gitprint.java
+++ gitprint.java
@@ -1,2 +1,9 @@
-System.out.println("HelloWorld");
-System.out.println("Pooja");
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+
+
+//System.out.println("HelloWorld");
+//System.out.println("Pooja");
